d8d26b037729380c4fd4f3fa8b094acd76d7ff68
Ahmed Hany <ahmedhanyhosny@outlook.com>
2022-09-17 20:53:56 +0200

    Create InputField component
    - Create and style InputField component.
    - Save the component file in src/component/InputField.js.
    - Save its styles in src/styles/InputField.css.

---
+++ src/components/InputField.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/InputField.css";
+
+class InputField extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { type, id, label } = this.props;
+
+    return (
+      <div className="input-wrapper">
+        <label htmlFor={id}>{label}</label>
+        <input type={type} id={id} name={id} />
+      </div>
+    );
+  }
+}
+
+export default InputField;
