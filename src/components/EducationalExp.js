e3248e7983377273bfd7cb25046727b7570b8636
Ahmed Hany <ahmedhanyhosny@outlook.com>
2022-09-17 22:59:56 +0200

    Create two more components
    - Create EducationExp component.
    - Create PracticalExp component.

---
+++ src/components/EducationalExp.js
@@ -0,0 +1,32 @@
+import React from "react";
+import InputField from "./InputField";
+
+class EducationalExp extends React.Component {
+  render() {
+    return (
+      <section>
+        <h2>Education Experience</h2>
+        <InputField
+          type="text"
+          id="school"
+          label="School Name"
+          required="true"
+        />
+        <InputField
+          type="text"
+          id="study"
+          label="Field of Study"
+          required="true"
+        />
+        <InputField
+          type="date"
+          id="date"
+          label="Date of Study"
+          required="true"
+        />
+      </section>
+    );
+  }
+}
+
+export default EducationalExp;
---
+++ src/components/PracticalExp.js
@@ -0,0 +1,18 @@
+import React from "react";
+import InputField from "./InputField";
+
+class PracticalExp extends React.Component {
+  render() {
+    return (
+      <section>
+        <h2>Practical Experience</h2>
+        <InputField type="text" id="company_name" label="Company Name" />
+        <InputField type="text" id="position" label="Position Title" />
+        <InputField type="date" id="start_date" label="Start date" />
+        <InputField type="date" id="end_date" label="End date" />
+      </section>
+    );
+  }
+}
+
+export default PracticalExp;
